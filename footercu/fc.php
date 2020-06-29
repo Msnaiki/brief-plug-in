f1ff7445d946ab931e12686e808c79c68b5436c3
mehdi snaiki <mehdisnaikidono2@gmail.com>
2020-06-29 19:47:34 +0100

    message

--- footercu/fc.php
+++ footercu/fc.php
@@ -11,6 +11,27 @@ Author URI: https://youtube.com
 if (!defined ('ABSPATH')){
     exit;
 }
+add_action('admin_menu', 'my_admin_menu');
+
+function my_admin_menu () {
+	//parameters details
+	 //add_management_page( $page_title, $menu_title, $capability,$menu_slug, $function );
+	 //add a new setting page udner setting menu
+  //add_management_page('Footer Text', 'Footer Text', 'manage_options', __FILE__, //'footer_text_admin_page');
+  //add new menu and its sub menu 
+  add_menu_page('Footer Text title', 'Footer customize', 'manage_options','footer_setting_page', 'footer_text_admin_page','dashicons-translation');
+add_submenu_page( 'footer_setting_page', 'Page title', 'Sub-menu title','manage_options', 'child-submenu-handle', 'my_magic_function');
+}
+
+function footer_text_admin_page () {
+  echo 'this is where we will edit the variable';
+}
+
+// mt_settings_page() displays the page content for the Test Settings submenu
+function mt_settings_page() {
+    echo "<h2>" . __( 'Footer Settings Configurations', 'menu-test' ) . "</h2>";
+	include_once('footer_settings_page.php');
+}
 //load script
 require_once(plugin_dir_path(__FILE__).'/includes/script.php');
 //load class
